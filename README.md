ffaed359c8fc3e4df8d50a68da878c315e302d3b
nikkiricks <nikki.ricks@gmail.com>
2020-08-10 15:25:56 +1000

    added logic to sum up donation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # antiracism-mern
 
-Please see my medium article for reference.
+Please see my [medium article](https://medium.com/weekly-webtips/building-restful-apis-with-node-js-and-express-a9f648219f5b) for reference.
--- frontend/src/Components/Donation/DonationList.js
+++ frontend/src/Components/Donation/DonationList.js
@@ -7,28 +7,10 @@ const DonationList = (props) => {
       <li className="collection-header"><h4>Donations</h4></li>
       {props.donations.map((item) => (
         <a href="#!" className="collection-item" key={item._id}
-        onClick={props.updateCurrentDonation.bind(this,item)}>{item.organizationName} </a>
+      onClick={props.updateCurrentDonation.bind(this,item)}>{item.organizationName} - ${item.dollarAmount}</a>
       ))}
     </ul>
-
-    <table>
-  <thead>
-    <tr>
-        <th>Organization Name</th>
-        <th>Money Donation</th>
-        <th>Comment</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-    {props.donations.map((item) => (
-        <td className="collection-item" key={item._id}
-        onClick={props.updateCurrentDonation.bind(this,item)}>{item.organizationName} </td>
-      ))}
-    </tr>
-  </tbody>
-  </table>
+      <h5>Total: $ {props.donations.reduce((totalDonations, donation) => totalDonations + donation.dollarAmount, 0)}</h5>
   </div> 
  );
 }
